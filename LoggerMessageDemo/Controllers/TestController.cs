6423fd33bf15c16f96c8dc8c7ec5b66855a09452
StefanTheCode <stefan.djokic1995@gmail.com>
2023-10-30 11:16:48 +0100

    Newsletter

---
+++ LoggerMessageDemo/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoggerMessageDemo.Controllers;
+public partial class TestController
+{
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [LoggerMessage(0, LogLevel.Information, "Writing hello world response to {Person}")]
+    partial void LogHelloWorld(Person person);
+
+    [HttpGet("/")]
+    public string Get()
+    {
+        var person = new Person(123, "Joe Blogs");
+
+        LogHelloWorld(person);
+
+        return "Hello world!";
+    }
+
+    public record Person(int Id, string Name);
+}
---
+++ LoggerMessageDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LoggerMessageDemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
